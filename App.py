20958d4b11890ae1df2b0f54c6879597fff83483
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-11-08 12:49:16 +0700

    Update App.py

--- App.py
+++ App.py
@@ -24,6 +24,6 @@ rows = run_query("SELECT * FROM `MA.Sensor` LIMIT 10")
 
 # Print results.
 st.write("Some wise words:")
-st.write(rows')
+st.write(rows)
 for row in rows:
     st.write("✍️ " + row['DO'])
