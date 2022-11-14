2117f830fd597e706cd00531146f776d91872761
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-11-14 21:34:39 +0700

    date

--- App.py
+++ App.py
@@ -56,7 +56,7 @@ def status_onlimo(id_ol):
         globals()[f'header_a_{id_ol}'].button(globals() [f'tgl_{id_ol}'], key=f'{id_ol}_a')
         globals()[f'header_b_{id_ol}'].success('ONLINE')
 
-    elif globals()[f'tgl_{id_ol}'] < datetime.today().date(): 
+    elif datetime.strptime(globals()[f'tgl_{id_ol}'].astype('str'), '%Y-%m-%d') < datetime.today().date(): 
         globals()[f'header_a_{id_ol}'].button(globals() [f'tgl_{id_ol}'], key=f'{id_ol}_b')
         globals()[f'header_b_{id_ol}'].warning('OFFLINE')
 
